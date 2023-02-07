14459d0a094614fa83cd6a2a8ecbbbae563208d6
Blakintosh <6821024+Blakintosh@users.noreply.github.com>
2023-02-07 19:49:05 +0000

    Improved model types

--- src/frontend/src/routes/dashboard/models.ts
+++ src/frontend/src/routes/dashboard/models.ts
@@ -1,20 +1,37 @@
+/**
+ * Legal RAG classifications. One of: unknown (n/a), normal (green), warning (amber), danger (red)
+ */
+export type RagClassification = "unknown" | "normal" | "warning" | "danger";
+
+/**
+ * Survey factor sub-model for elements of a SurveySummary.
+ */
 export type SurveyFactor = {
     name: string,
     description: string,
     satisfaction: number,
-    status: string
+    status: RagClassification
 }
 
+/**
+ * Survey summary tile sub-model of a SoftwareProject.
+ */
 export type SurveySummary = {
     date: Date,
     factors: Array<SurveyFactor>
 }
 
+/**
+ * Health summary tile sub-model of a SoftwareProject.
+ */
 export type HealthSummary = {
-    status: string,
+    status: RagClassification,
     issues: number
 }
 
+/**
+ * Primary model for the API request for a project.
+ */
 export type SoftwareProject = {
     name: string
     health: HealthSummary,
