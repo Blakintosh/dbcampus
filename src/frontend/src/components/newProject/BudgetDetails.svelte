3afb3a78dc52650d7853a3b3eae1d8aef5e97620
Blakintosh <6821024+Blakintosh@users.noreply.github.com>
2023-03-12 13:06:19 +0000

    Pain

---
+++ src/frontend/src/components/newProject/BudgetDetails.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import FormField from "../form/FormField.svelte";
+	import FormTextField from "../form/FormTextField.svelte";
+</script>
+
+<h1 class="text-xl">Budget Details</h1>
+
+<p class="text-sm">
+	Please fill out all details relating to the budget of the project and its expected costs.
+</p>
+<p class="text-sm">
+	<span class="text-red-600 font-semibold">*</span> = Mandatory
+</p>
+
+<FormField label="What currency are you using?" isFor="currency" required>
+	<select name="currency" class="w-full border border-slate-300 rounded-lg p-2 mt-2">
+		<option value="GBP">GBP</option>
+		<option value="USD">USD</option>
+		<option value="EUR">EUR</option>
+	</select>
+</FormField>
+
+<FormField label="What is the project budget?" isFor="budget" required>
+	<input type="number" name="budget" placeholder="Budget" min="0" class="w-full border border-slate-300 rounded-lg p-2 mt-2"/>
+</FormField>
+
+<FormField label="What's your expected regular monthly expenditure?" isFor="monthlyExpenditure" required>
+	<input type="number" name="monthlyExpenditure" placeholder="Monthly Expenditure" min="0" class="w-full border border-slate-300 rounded-lg p-2 mt-2"/>
+</FormField>
+
+<FormField label="Are there any costs up to now that you would like to add?" isFor="initialCost">
+	<input type="number" name="initialCost" placeholder="Initial Expenses" min="0" class="w-full border border-slate-300 rounded-lg p-2 mt-2"/>
+</FormField>
--- src/frontend/src/routes/dashboard/new/+page.svelte
+++ src/frontend/src/routes/dashboard/new/+page.svelte
@@ -6,6 +6,10 @@
 	import MultiStageForm from "../../../components/form/MultiStageForm.svelte";
 	import FormTextField from "../../../components/form/FormTextField.svelte";
 	import FormField from "../../../components/form/FormField.svelte";
+	import ProjectDetails from "../../../components/newProject/ProjectDetails.svelte";
+	import BudgetDetails from "../../../components/newProject/BudgetDetails.svelte";
+	import ActionButton from "../../../components/common/ActionButton.svelte";
+	import ConnectionsDetails from "../../../components/newProject/ConnectionsDetails.svelte";
 
     const stages = [
         "Details",
@@ -13,6 +17,12 @@
         "Connections",
         "Done"
     ];
+
+	const stageComponents = [
+		ProjectDetails,
+		BudgetDetails,
+		ConnectionsDetails
+	]
 </script>
 
 <svelte:head>
@@ -32,29 +42,20 @@
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="fixed w-full h-full bg-black/60 grid z-50 backdrop-blur-sm overflow-hidden">
     </div>
-    <div class="fixed w-[40%] h-[75%] translate-x-[-50%] left-[50%] top-[50%] translate-y-[-50%] z-50 bg-slate-200 border border-slate-300 text-black shadow-xl rounded-lg flex flex-col items-stretch overflow-hidden">
+    <div class="fixed w-[35%] h-[75%] translate-x-[-50%] left-[50%] top-[50%] translate-y-[-50%] z-50 bg-slate-200 border border-slate-300 text-black shadow-xl rounded-lg flex flex-col items-stretch overflow-hidden">
         <div class="px-8 py-3 border-b border-slate-300 bg-slate-100 shrink-0">
             <PageHeading section="Dashboard" title="New Project"/>
         </div>
         <div class="flex-grow p-4 flex flex-col items-center gap-8">
             <div class="flex items-center justify-center mt-8">
-                <MultiStageForm stage={1} {stages}/>
+                <MultiStageForm stage={2} {stages}/>
             </div>
             <div class="w-[80%]">
-                <h1 class="text-xl">Basic Details</h1>
-
-                <p class="text-sm">
-                    If you don't know what to put in here, don't worry. You can always change it later.
-                </p>
-                <p class="text-sm">
-                    <span class="text-red-600 font-semibold">*</span> = Mandatory
-                </p>
-                
-                <FormTextField label="What is the project called?" placeholder="Project Name" required name="projectName"/>
-
-                <FormField label="When is the project set to be finished?" isFor="finishDate" required>
-                    <input type="date" name="finishDate" class="w-full border border-slate-300 rounded-lg p-2 mt-2"/>
-                </FormField>
+                <svelte:component this={stageComponents[2]}/>
+				
+				<div class="flex flex-col w-full mt-6">
+					<ActionButton label="Next" loading={false} />
+				</div>
             </div>
         </div>
     </div>
