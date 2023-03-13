fa37a673257119d42b6b6d01d2725f3283ad62d6
Blakintosh <6821024+Blakintosh@users.noreply.github.com>
2023-03-13 02:46:53 +0000

    push

--- src/frontend/src/components/dashboard/sidebar/SideNav.svelte
+++ src/frontend/src/components/dashboard/sidebar/SideNav.svelte
@@ -13,6 +13,7 @@
 	import { onMount } from "svelte";
 	import { fly } from "svelte/transition";
 	import PageHeading from "../../common/PageHeading.svelte";
+	import { goto } from "$app/navigation";
 
 	export let projects: Array<SoftwareProjectSnippet> = [];
 	
@@ -68,7 +69,7 @@
 				<h3 class="text-xs font-semibold mb-2">Go to Project</h3>
 				<ProjectsDropdown {projects}/>
 
-				<button class="flex items-center justify-center fill-amber-400 text-amber-400 font-semibold text-sm gap-3 mb-8 mx-2">
+				<button class="flex items-center justify-center fill-amber-400 text-amber-400 hover:text-amber-500 hover:fill-amber-500 font-semibold text-sm gap-3 mb-8 mx-2" on:click={() => goto("/dashboard/new")}>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-4 h-4"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M240 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H176V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H384c17.7 0 32-14.3 32-32s-14.3-32-32-32H240V80z"/></svg>
 					Start New Project
 				</button>
